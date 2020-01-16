e1d5bb2efd76e6489387e324e2c4c825e756540e
Student <example@example.com>
2020-01-16 10:56:07 -0800

    add if else statements to adjust quote

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,20 @@ $(document).ready(function() {
     $("form#insurance").submit(function(event) {
       var age = parseInt($("input#age").val());
       var gender = $("select#gender").val();
-  
-      var quote = (100 - age) * 3;
+
+      if (age) {
+          var quote = (100 - age) * 3;
+        if (gender === 'male' || age < 26) {
+            quote +=50;
+          
+      }
   
       $("#rate").empty().append(quote);
+    //   $("#rate").text(quote);
       $("#quote").show();
+    } else {
+        alert("Please enter your age.");
+    }
   
       event.preventDefault();
     });
